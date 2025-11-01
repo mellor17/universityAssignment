5c4223f4426bbd67462415653ffa965070e8c108
Ben Mellor <ben.mellor@booking.com>
2025-11-01 09:58:35 +0000

    fix(BM): commented out private InputUtility constructor as its likely overkill, I don't think I will instantiate a new instance of the class

--- src/main/java/physicsCalculator/InputUtility.java
+++ src/main/java/physicsCalculator/InputUtility.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class InputUtility {
     public static final Scanner scanner = new Scanner(System.in); // implemented this so i don't have to keep declaring scanner each time and can just reference it \_//_/ ⍩
-    private InputUtility() {
-    // this is just error handling so I can't accidentally create a new instance of the class and overwrite the variable causing errors in the rest of the program
-    }
+//    private InputUtility() {
+//    // this is just error handling so I can't accidentally create a new instance of the class and overwrite the variable causing errors in the rest of the program
+//    }
 }
